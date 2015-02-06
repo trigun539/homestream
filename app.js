ef2f1a3a77b55ca9f9331bc7e601483e25017760
Edwin Perez <edwinperez539@gmail.com>
2015-02-06 02:33:07 -0500

    Changed port.

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ app.get('/*', function(req, res, next){
 /**
 *	Initializing Server
 */ 
-var server = app.listen(46005, function () {
-  console.log('App listening at http://localhost:46005');
+var server = app.listen(3000, function () {
+  console.log('App listening at http://localhost:3000');
 });
